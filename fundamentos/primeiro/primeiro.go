59a60768ec3754de2965284e8439a5a8ec915b06
sidney <dev18@redepos.com.br>
2020-02-11 08:34:30 -0300

    incluir comentarios

--- fundamentos/primeiro/primeiro.go
+++ fundamentos/primeiro/primeiro.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+// programa executavel iniciam pelo pacote main
 
+import (
+	"fmt"
+)
+
+// A porta de entrda de um programa GO é a função main
+func main() {
+	fmt.Println("Ola Mundo!")
+	fmt.Printf("Ola Mundo em %s!", "go")
+
+	/*
+		Sobre comentarios
+
+		1) Priorize codigo legivel e faça comentarios que agrega valor
+		2) Evite comentarios óbvios
+		3) Durante o curso abuse dos comentários
+	*/
 }
