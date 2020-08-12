0650e7952f74899ae4ed5e955969fdb0f6ba6284
sidney <dev18@redepos.com.br>
2020-08-12 11:29:40 -0300

    implementando rabbitMQ

---
+++ src/rabbitmqex/receive.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+func failOnError2(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
+func main() {
+
+	fmt.Println("Cosumer Application")
+
+	// connect to RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/") // amqp://guest:guest@localhost:5672/")
+	failOnError2(err, "Failed to connect to RabbitMQ")
+	// close
+	defer conn.Close()
+
+	fmt.Println("Sucessfully Connected To our RabbitMQ Instance")
+	ch, err := conn.Channel()
+	failOnError2(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError2(err, "Failed to declare a queue")
+	fmt.Println(q)
+
+	msgs, err := ch.Consume(
+		q.Name, // queue
+		"",     // consumer
+		true,   // auto-ack
+		false,  // exclusive
+		false,  // no-local
+		false,  // no-wait
+		nil,    // args
+	)
+	failOnError2(err, "Failed to register a consumer")
+
+	forever := make(chan bool)
+	go func() {
+		for d := range msgs {
+			log.Printf("Received a message: %s", d.Body)
+		}
+	}()
+
+	log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	<-forever
+}
---
+++ src/rabbitmqex/send.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	//"flag"
+	"fmt"
+	"github.com/streadway/amqp"
+	"log"
+	"time"
+)
+
+func main() {
+
+	// connect to RabbitMQ server
+	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672") // amqp://guest:guest@localhost:5672/")
+	// conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	// close
+	fmt.Println("Sucessfully Connected To our RabbitMQ Instance")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		"hello", // name
+		false,   // durable
+		false,   // delete when unused
+		false,   // exclusive
+		false,   // no-wait
+		nil,     // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	fmt.Println(q)
+
+	body := "Hello Word ONE!"
+	err = ch.Publish(
+		"",     // exchange
+		q.Name, // routing key
+		false,  // mandatory
+		false,  // immediate
+		amqp.Publishing{
+			Headers:         nil,
+			ContentType:     "text/plain",
+			ContentEncoding: "",
+			DeliveryMode:    0,
+			Priority:        0,
+			CorrelationId:   "",
+			ReplyTo:         "",
+			Expiration:      "",
+			MessageId:       "",
+			Timestamp:       time.Time{},
+			Type:            "",
+			UserId:          "",
+			AppId:           "",
+
+			Body: []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+}
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Fatalf("%s: %s", msg, err)
+	}
+}
